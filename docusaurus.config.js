b13059ca563162bdebfe11a7cfaf2e918d8b5192
kuma@gmo-cybersecurity.com <kuma@gmo-cybersecurity.com>
2024-11-14 20:57:35 +0900

    fix deadlink

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: 'https://kuma3ierae.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/pages_test/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -146,6 +146,7 @@ const config = {
       prism: {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
+        additionalLanguages: ['diff'],
       },
     }),
 };
