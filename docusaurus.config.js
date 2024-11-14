36a110e16bf2a2d4ea890218cb9b213ac09ce726
kuma@gmo-cybersecurity.com <kuma@gmo-cybersecurity.com>
2024-11-14 20:34:24 +0900

    updated

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,12 +18,12 @@ const config = {
   url: 'https://kuma3ierae.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/pages_test/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'kuma3ierae', // Usually your GitHub org/user name.
-  projectName: 'pages_test',
+  projectName: 'kuma3ierae.github.io',
   deploymentBranch: 'main',
   trailingSlash: false,
 
